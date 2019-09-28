f7e9df0d32779a62ced351b70d2540f0fb3e8446
androidmalin <malin.myemail@163.com>
2019-09-28 13:48:30 +0800

    add get dependencies shell

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf node_modules && \
+rm -rf ~/.rncache && \
+rm -rf ios/Pods && \
+rm -rf android/.gradle && \
+rm -rf android/app/build && \
+yarn cache clean && \
+npm install  && \
+cd ios  && \
+pod install  && \
+cd ..
