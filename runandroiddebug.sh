a5c31a78cc9e8a8d152d8eb725d723213fa92109
androidmalin <malin.myemail@163.com>
2019-10-02 23:59:59 +0800

    add debug release run shell

--- runandroiddebug.sh
+++ runandroiddebug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 first_device=`adb devices | awk  'NR==2' | awk  '{print $1}'`
+packageName="com.reactnativedemo"
+adb uninstall $packageName
 react-native run-android --deviceId $first_device
